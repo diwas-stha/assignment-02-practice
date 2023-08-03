9cafb4c1efb97a4f725fce7b1cbfbad8746cb93a
diwas-stha <diwas@fusemachines.com>
2023-08-03 17:55:37 +0545

    fix: added docstring

--- 2/reduce.py
+++ 2/reduce.py
@@ -1,6 +1,15 @@
 from functools import reduce
 
 def concatenate_strings(strings):
+    '''
+    Concatenates the list of string to return single string
+
+    Parameter:
+        list(str) : takes in a list of strings
+
+    Returns
+        string: a single string containing concatenation of all the elements
+    '''
     concat = reduce((lambda x, y: x + y), strings)
     return concat
 
